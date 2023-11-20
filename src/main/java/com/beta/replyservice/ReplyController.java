20f6c9ee17300667912d13e2744069e662ca3a54
Geetik Nimonkar <geetiknimonkar8@gmail.com>
2023-11-21 01:16:10 +0530

    Updated logic for invalid inputs and added logs

--- src/main/java/com/beta/replyservice/ReplyController.java
+++ src/main/java/com/beta/replyservice/ReplyController.java
@@ -1,6 +1,10 @@
 package com.beta.replyservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,20 +14,30 @@ import org.springframework.web.server.ResponseStatusException;
 public class ReplyController {
 
 	@Autowired
-	private RestService restService;
+	private ReplyService replyService;
+
+	Logger logger = LoggerFactory.getLogger(ReplyController.class);
 
 	@GetMapping("/reply")
 	public ReplyMessage replying() {
+		logger.info("Reply service V1 is called with empty message string");
 		return new ReplyMessage("Message is empty");
 	}
 
 	@GetMapping("/reply/{message}")
 	public ReplyMessage replying(@PathVariable String message) {
+		logger.info("Reply service V1 is called with message string : " + message);
 		return new ReplyMessage(message);
 	}
 
 	@GetMapping("/v2/reply/{message}")
-	public ReplyMessage replyingMessageWithRules(@PathVariable String message) throws ResponseStatusException {
-		return restService.getReplyWithRules(message);
+	public ResponseEntity<ReplyMessage> replyingMessageWithRules(@PathVariable String message) throws ResponseStatusException {
+		logger.info("Reply service V2 is called with message string : " + message);
+		try {
+			return new ResponseEntity<>(replyService.getReplyWithRules(message), HttpStatus.OK) ;
+		}
+		catch (ResponseStatusException ex) {
+			return new ResponseEntity<>(new ReplyMessage("Invalid input"), HttpStatus.BAD_REQUEST) ;
+		}
 	}
 }
--- src/main/java/com/beta/replyservice/ReplyService.java
+++ src/main/java/com/beta/replyservice/ReplyService.java
@@ -1,6 +1,8 @@
 package com.beta.replyservice;
 
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -10,7 +12,7 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 @Service
-public class RestService {
+public class ReplyService {
 
     final String INVALID_INPUT = "Invalid input";
     final String EMPTY_STRING = "";
@@ -19,15 +21,21 @@ public class RestService {
     final int HYPHEN_INDEX = 2;
     final int TEXT_STARTING_INDEX = 3;
 
+    Logger logger = LoggerFactory.getLogger(ReplyService.class);
+
     public ReplyMessage getReplyWithRules(final String message) throws ResponseStatusException {
+
+        logger.info("getReplyWithRules called with message : " + message);
 
         int lengthOfInputText = message.length();
 
         if(lengthOfInputText < 3) {
+            logger.info("Message length not valid");
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, INVALID_INPUT);
         }
 
         if(message.charAt(HYPHEN_INDEX) != '-') {
+            logger.info("Delimeter not present at valid index");
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, INVALID_INPUT);
         }
 
@@ -46,6 +54,8 @@ public class RestService {
     }
 
     private String changeAccordingToRule(final String text,final char rule) throws ResponseStatusException {
+
+        logger.info("changeAccordingToRule called for " + text + " and rule : " + rule);
 
         String updatedText = EMPTY_STRING;
 
@@ -69,6 +79,7 @@ public class RestService {
     }
 
     private String encodeInputString(@NotNull final String str) throws NoSuchAlgorithmException {
+        logger.info("encodeInputString called for " + str);
         MessageDigest md = MessageDigest.getInstance("MD5");
         byte[] digest = md.digest(str.getBytes());
         BigInteger no = new BigInteger(1, digest);
@@ -81,6 +92,7 @@ public class RestService {
     }
 
     private String reverseString(@NotNull final String str) {
+        logger.info("reverseString called for " + str);
         StringBuilder sb = new StringBuilder(str);
         sb.reverse();
         return sb.toString();
